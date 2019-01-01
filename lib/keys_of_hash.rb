dd35d183ff93c048af6745452cdc9815d3d8dea1
Kyle Houghton <kylebenz@hotmail.com>
2019-01-01 17:07:53 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@
 class Hash
   def keys_of(*arguments)
     return_this = []
-    self.collect{|key, value| return_this << key if arguments.include?(value)}
+    self.each{|key, value| return_this << key if arguments.include?(value)}
     return_this
   end
 end
