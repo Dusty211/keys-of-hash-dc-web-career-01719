d25008ef8845aae93ba9db89cf8d11bae28ad4e5
Kyle Houghton <kylebenz@hotmail.com>
2019-01-01 17:07:01 +0000

    All tests pass.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,8 @@
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+    return_this = []
+    self.collect{|key, value| return_this << key if arguments.include?(value)}
+    return_this
   end
 end
